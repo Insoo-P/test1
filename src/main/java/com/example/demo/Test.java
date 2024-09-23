6f005caaafda83242e6130075b0aab3c509fc0e3
박인수 <insoodever@gmail.com>
2024-09-23 20:55:28 +0900

    2 coomit

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요1");
+        System.out.println("안녕하세요2");
+        System.out.println("안녕하세요3");
+        System.out.println("안녕하세요4");
+        System.out.println("안녕하세요5");
+    }
+}
