bd9b9082a484100071959ee2d1156793ec7981ca
박인수 <insoodever@gmail.com>
2024-09-24 00:26:25 +0900

    3 commit

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -5,7 +5,8 @@ public class Test {
         System.out.println("안녕하세요1");
         System.out.println("안녕하세요2");
         System.out.println("안녕하세요3");
-        System.out.println("안녕하세요4");
+
+        System.out.println("안녕하세요8");
         System.out.println("안녕하세요5");
     }
 }
